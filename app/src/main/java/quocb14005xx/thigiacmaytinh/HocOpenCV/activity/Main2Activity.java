ba57f5d4b7d6519f2ebd8c1899acbda052bcb3a0
quocb14005xx <quocb14005xx@gmail.com>
2018-01-12 22:12:41 +0700

    abaab

--- app/src/main/java/quocb14005xx/thigiacmaytinh/HocOpenCV/activity/Main2Activity.java
+++ app/src/main/java/quocb14005xx/thigiacmaytinh/HocOpenCV/activity/Main2Activity.java
@@ -4,14 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
@@ -21,53 +18,64 @@ import java.io.InputStream;
 
 import quocb14005xx.thigiacmaytinh.HocOpenCV.R;
 import quocb14005xx.thigiacmaytinh.HocOpenCV.object.MyContants;
+import quocb14005xx.thigiacmaytinh.HocOpenCV.view.MyImageView;
 
 
 //Main2Activity xử lý với ảnh từ thư viện
-public class Main2Activity extends AppCompatActivity implements View.OnTouchListener{
+public class Main2Activity extends AppCompatActivity {
     private Bitmap bitmap_root;//xử lý trên ảnh này
-    private ImageView imgSHOW;//show trong view này
+    private MyImageView imageView;//show trong view này
 
-    private int REQUEST_CODE=1234;
-
-
+    private int REQUEST_CODE = 1234;
+    //click Quay ảnh
+    private int deg;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             try {
-                InputStream stream= getContentResolver().openInputStream(data.getData());
+                InputStream stream = getContentResolver().openInputStream(data.getData());
                 bitmap_root = BitmapFactory.decodeStream(stream);
-                imgSHOW.setImageBitmap(bitmap_root);
+                imageView.setImageBitmap(bitmap_root);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         //load anh goc
         LayAnhTuIntroActivity();
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.background_intro);
+         imageView = findViewById(R.id.imageShow);
+        imageView.setImageBitmap(bitmap);
+
     }
+
+    /**
+     * can lay bitmap dang ve ra thi goi {@link MyImageView#getBitmap()}
+     */
+
     //lấy ảnh từ intent bên intro trong thư viện qua đây
-    private void LayAnhTuIntroActivity()
-    {
+    private void LayAnhTuIntroActivity() {
         Uri dataBitmap = Uri.parse(getIntent().getStringExtra("image_selected"));
         try {
             InputStream stream = getContentResolver().openInputStream(dataBitmap);
             bitmap_root = BitmapFactory.decodeStream(stream);
-            if(bitmap_root.getWidth()>1280 || bitmap_root.getHeight()>1280)//nếu ảnh đầu vào mà size lớn quá thì resize lại để xử lý
+            if (bitmap_root.getWidth() > 1280 || bitmap_root.getHeight() > 1280)//nếu ảnh đầu vào mà size lớn quá thì resize lại để xử lý
             {
-                bitmap_root=Bitmap.createScaledBitmap(bitmap_root,1280,960,true);
+                bitmap_root = Bitmap.createScaledBitmap(bitmap_root, 1280, 960, true);
             }
 
             stream.close();
-            imgSHOW= findViewById(R.id.imageShow);
+            imageView = findViewById(R.id.imageShow);
 
-            imgSHOW.setImageBitmap(bitmap_root);
+            imageView.setImageBitmap(bitmap_root);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -76,34 +84,35 @@ public class Main2Activity extends AppCompatActivity implements View.OnTouchList
     //click Undo
     public void XuLyUnDo(View view) {
     }
+
     //click Redo
     public void XuLyReDo(View view) {
 
     }
-    //click Quay ảnh
-    private int deg;
+
     public void XuLyQuayAnh(View view) {
         Matrix matrix = new Matrix();
-        matrix.postRotate(deg+90);
-        bitmap_root=Bitmap.createBitmap(bitmap_root,0,0,bitmap_root.getWidth(),bitmap_root.getHeight(),matrix,true);
-        imgSHOW.setImageBitmap(bitmap_root);
+        matrix.postRotate(deg + 90);
+        bitmap_root = Bitmap.createBitmap(bitmap_root, 0, 0, bitmap_root.getWidth(), bitmap_root.getHeight(), matrix, true);
+        imageView.setImageBitmap(bitmap_root);
     }
+
     //click save ảnh đã chỉnh sửa
-    public void XuLySaveBitmap(View view)
-    {
+    public void XuLySaveBitmap(View view) {
         try {
             FileOutputStream outputStream = new FileOutputStream(MyContants.RecordHinhAnh_filepath());
-            bitmap_root.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
-            bitmap_root.recycle();
-            bitmap_root=null;
+            Bitmap bitmap = imageView.getBitmap();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+           /* bitmap.recycle();*/ //->crash
+
             Toast.makeText(this, "Đã lưu rồi", Toast.LENGTH_SHORT).show();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
+
     //click để thay đổi ảnh khác
-    public void XuLyThayDoiAnhKhac(View view)
-    {
+    public void XuLyThayDoiAnhKhac(View view) {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
@@ -114,13 +123,5 @@ public class Main2Activity extends AppCompatActivity implements View.OnTouchList
     public void XuLyCutAnh(View view) {
 
     }
-    Canvas canvas = new Canvas(bitmap_root);
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        if (view.getId()==R.id.imageShow)
-        {
 
-        }
-        return true;
-    }
 }
--- app/src/main/java/quocb14005xx/thigiacmaytinh/HocOpenCV/view/MyImageView.java
+++ app/src/main/java/quocb14005xx/thigiacmaytinh/HocOpenCV/view/MyImageView.java
@@ -1,15 +1,17 @@
 package quocb14005xx.thigiacmaytinh.HocOpenCV.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -19,9 +21,12 @@ import quocb14005xx.thigiacmaytinh.HocOpenCV.object.MyContants;
  * Created by quocb14005xx on 1/12/2018.
  */
 
-public class MyImageView extends AppCompatImageView {
-    Paint mPaint;
-    ArrayList<Point> listPoint;
+public class MyImageView extends View {
+    private final Object mLock = new Object();
+    private Paint mPaint;
+    private ArrayList<Point> mListPoint;
+    @Nullable
+    private Bitmap mBitmap;
 
     public MyImageView(Context context) {
         super(context);
@@ -38,28 +43,43 @@ public class MyImageView extends AppCompatImageView {
         initPaint(context);
     }
 
-    private void initPaint(Context ctx) {
-        listPoint = new ArrayList<>();
+    private void initPaint(Context context) {
+        mListPoint = new ArrayList<>();
         mPaint = new Paint();
         mPaint.setStrokeWidth(5);
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setColor(Color.RED);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        for (Point p : listPoint) {
-            canvas.drawPoint(p.x, p.y, mPaint);
+        synchronized (mLock) {
+            canvas.drawBitmap(mBitmap, 0, 0, mPaint);
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         Log.e(MyContants.TAG, event.getX() + "----" + event.getY());
-        listPoint.add(new Point(Math.round(event.getX()), Math.round(event.getY())));
-        invalidate();
+        /*mListPoint.add(new Point(Math.round(event.getX()), Math.round(event.getY())));*/
+        if (mBitmap != null) {
+            synchronized (mLock) {
+                Canvas canvas = new Canvas(mBitmap);
+                canvas.drawPoint(event.getX(), event.getY(), mPaint);
+            }
+            invalidate();
+        }
         return true;
     }
 
+    @Nullable
+    public Bitmap getBitmap() {
+        return mBitmap;
+    }
 
+    public void setImageBitmap(@NonNull Bitmap bitmap) {
+        this.mBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+    }
 }
